d0c1da97210254920276b3409c39bd4d56e55219
Maxime <Maxime.Pochet@hotmail.fr>
2018-12-19 17:15:55 +0100

    NavBar
    Create navBar

--- frontend/src/routes/PublicRouter.js
+++ frontend/src/routes/PublicRouter.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 import {Switch, Route, withRouter} from 'react-router-dom';
 import Profile from '../components/Profile';
 import { connect } from 'react-redux';
+import Page2 from '../pages/Page2';
 
 class AppRouter extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={Profile}/>
+        {/* <Route exact path="/" component={Profile}/> */}
+        <Route exact path="/" component={Page2}/>
+
       </Switch>
     )
   }

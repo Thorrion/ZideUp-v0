2cc869e5ff5ecdfd0016001d82ff1cc4c3d99221
Maxime <Maxime.pochet@hotmail.fr>
2018-12-20 08:18:50 +0100

    Lost icons

--- frontend/src/components/CreateDefi/CreateDefi.jsx
+++ frontend/src/components/CreateDefi/CreateDefi.jsx
@@ -11,7 +11,6 @@ import Button from '@material-ui/core/Button';
 import './CreateDefi.scss'
 
 
-
 const styles = theme => ({
   container: {
     display: 'flex',
@@ -19,6 +18,11 @@ const styles = theme => ({
     paddingTop: '10vh'
   },
   input: {
+    margin: theme.spacing.unit,
+    width: '90vw',
+    marginLeft: '5vw'
+  },
+  input2: {
     margin: theme.spacing.unit,
     width: '90vw',
     marginLeft: '5vw'
@@ -32,7 +36,7 @@ const styles = theme => ({
   textField2: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: '40vw',
+    width: '41vw',
     marginLeft: '5vw'
   },
   dense: {
@@ -78,126 +82,174 @@ const currencies = [
 
 class CreateDefi extends Component {
   render() {
-      const { classes } = this.props;
-      return (
-        <React.Fragment>
-      <Navbar/>
+    const { classes } = this.props;
+    return (
 
-      <div className={classes.container}>
-      
-      <Input
-        placeholder="Nom du défi"
-        className={classes.input}
-        inputProps={{
-          'aria-label': 'Description',
-        }}
-        />
-      <TextField
-        id="standard-select-currency-native"
-        select
-        label="Périmètre concerné"
-        className={classes.textField}
-        // value={this.state.currency}
-        // onChange={this.handleChange('currency')}
-        SelectProps={{
-          native: true,
-          MenuProps: {
-            className: classes.menu,
-          },
-        }}
-        // helperText="Please select your currency"
-        margin="normal"
-        >
-        {currencies.map(option => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </TextField>
-      <Input
-        placeholder="Indicateur clé de performance"
-        className={classes.input}
-        inputProps={{
-          'aria-label': 'Description',
-        }}
-        />
-      <TextField
-          label="Actuel"
-          id="simple-start-adornment"
-          className={classes.textField2}
-          InputProps={{
-            endAdornment: <InputAdornment position="end">%</InputAdornment>,
-          }}
-          />
-      <TextField
-          label="Cible"
-          id="simple-start-adornment"
-          className={classes.textField2}
-          InputProps={{
-            endAdornment: <InputAdornment position="end">%</InputAdornment>,
-          }}
-          />
-        <form className={classes.container} noValidate>
-          <TextField
-            id="date"
-            label="Début"
-            type="date"
-            defaultValue="2017-05-24"
-            className={classes.textField2}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            />
-        </form>
-        <form className={classes.container} noValidate>
-          <TextField
-            id="date"
-            label="Fin"
-            type="date"
-            defaultValue="2018-05-24"
-            className={classes.textField2}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            />
-        </form>
-        <p>Sondage</p>
-        <TextField
-          id="standard-multiline-static"
-          label="Votre question"
-          multiline
-          rows="1"
-          className={classes.textField}
-          margin="normal"
-          />
-        <span class="fa fa-star checked"></span><p>Insuffisant</p>         <span class="fa fa-star checked"></span><p>Moyen</p>
-        <span class="fa fa-star checked"></span><p>Bien</p>
-        <span class="fa fa-star checked"></span><p>Excellent</p>
-      <Chips/>
-      <p>Commentaires</p>
-      <TextField
-        className={classes.margin}
-        id="input-with-icon-textfield"
-        // label="Ajoutez un commentaire"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <AccountCircle />
-            </InputAdornment>
-          ),
-        }}
-        />
-      <Button variant="contained" size="large" color="primary" className={classes.margin2}>
-          Enregistrer
-        </Button>
-      <Button variant="contained" size="large" color="primary" className={classes.margin2}>
-          Lancer le défi
-        </Button>
-      </div>
-    </React.Fragment>
-  );
-}
+      <React.Fragment>
 
+{/* NAVBAR */}
+
+        <Navbar/>
+
+          <div className={classes.container}>
+
+{/* NOM DU DEFI */}
+
+            <i class="fab fa-safari"></i>
+            <Input
+              placeholder="Nom du défi"
+              className={classes.input2}
+              inputProps={{
+                'aria-label': 'Description',
+              }}
+            />
+
+{/* PERIMETRE CONCERNE */}
+
+            <i class="fas fa-chart-line"></i>
+            <TextField
+              id="standard-select-currency-native"
+              select
+              label="Périmètre concerné"
+              className={classes.input2}
+              // value={this.state.currency}
+              // onChange={this.handleChange('currency')}
+              SelectProps={{
+                native: true,
+                MenuProps: {
+                  className: classes.menu,
+                },
+              }}
+              // helperText="Please select your currency"
+              margin="normal"
+            >
+              {currencies.map(option => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </TextField>
+
+{/* INDICATEUR DE PERFORMANCE */}
+
+            <i class="fas fa-vector-square"></i>
+            <Input
+              placeholder="Indicateur clé de performance"
+              className={classes.input2}
+              inputProps={{
+                'aria-label': 'Description',
+              }}
+            />
+
+{/* ACTUEL */}
+
+            <TextField
+              label="Actuel"
+              id="simple-start-adornment"
+              className={classes.textField2}
+              InputProps={{
+                endAdornment: <InputAdornment position="end">%</InputAdornment>,
+              }}
+            />
+          
+{/* CIBLE */}
+          
+            <TextField
+              label="Cible"
+              id="simple-start-adornment"
+              className={classes.textField2}
+              InputProps={{
+                endAdornment: <InputAdornment position="end">%</InputAdornment>,
+              }}
+            />
+
+{/* DEBUT */}
+
+            <form className={classes.container} noValidate>
+              <TextField
+                id="date"
+                label="Début"
+                type="date"
+                defaultValue="2017-05-24"
+                className={classes.textField2}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                />
+            </form>
+
+{/* FIN */}
+
+            <form className={classes.container} noValidate>
+              <TextField
+                id="date"
+                label="Fin"
+                type="date"
+                defaultValue="2018-05-24"
+                className={classes.textField2}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                />
+            </form>
+
+            <p>Sondage</p>
+
+{/* VOTRE QUESTION */}
+
+            <TextField
+              id="standard-multiline-static"
+              label="Votre question"
+              multiline
+              rows="1"
+              className={classes.textField}
+              margin="normal"
+              />
+
+{/* STARS RATE */}
+
+            <span class="fa fa-star checked"></span><p>Insuffisant</p>         <span class="fa fa-star checked"></span><p>Moyen</p>
+            <span class="fa fa-star checked"></span><p>Bien</p>
+            <span class="fa fa-star checked"></span><p>Excellent</p>
+
+{/* CHIPS */}
+          
+            <Chips/>
+
+            <p>Commentaires</p>
+            
+{/* COMMENTAIRE */}
+            
+            <TextField
+              className={classes.margin}
+              id="input-with-icon-textfield"
+              // label="Ajoutez un commentaire"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AccountCircle />
+                  </InputAdornment>
+                ),
+              }}
+            />
+
+{/* BUTTON ENREGISTRER */}
+
+            <Button variant="contained" size="large" color="primary" className={classes.margin2}>
+                Enregistrer
+            </Button>
+
+{/* BOUTON LANCER UN DEFI */}
+
+            <Button variant="contained" size="large" color="primary" className={classes.margin2}>
+              Lancer le défi
+            </Button>
+
+          </div>
+
+      </React.Fragment>
+    );
+  }
 }
 
 CreateDefi.propTypes = {
